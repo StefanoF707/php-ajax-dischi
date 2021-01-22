66604494e56657ace333cb10e76359e6b8c8b51e
Stefano Franchini <stefano.franchini77@gmail.com>
2021-01-22 15:53:50 +0100

    header

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    include __DIR__ . "/partials/db.php";
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,11 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
     <title>Albums</title>
 </head>
 <body>
 
-    <h1>ciao</h1>
+    <?php include __DIR__ . "/partials/header.php" ?>
+
 
 </body>
 </html>
---
+++ partials/header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="header-left">
+        <img src="img/logo-spotify.png" alt="logo Spotify" class="img">
+    </div>
+    <div class="header-right">
+        <select class="" name="">
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+        </select>
+    </div>
+</header>
